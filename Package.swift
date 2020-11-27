3eee6ff97b507549c199ae51b2180a4ccfe2ac59
Richard Bao <richardbao2000@gmail.com>
2020-11-27 11:31:55 -0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "ATGMediaBrowser",
+    products: [
+        .library(
+            name: "ATGMediaBrowser",
+            targets: ["ATGMediaBrowser"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "ATGMediaBrowser",
+            path: "ATGMediaBrowser",
+            exclude: ["Info.plist"]
+        )
+    ],
+    swiftLanguageVersions: [
+        .v4_2
+    ]
+)
